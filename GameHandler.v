80e67b13469c907ad4a963f1d092e75f59e3e42f
msisk23 <60163432+msisk23@users.noreply.github.com>
2021-12-04 17:13:11 -0500

    Initial game handler

---
+++ GameHandler.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/03/2021 07:01:05 PM
+// Design Name: 
+// Module Name: GameHandler
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module GameHandler(
+    input countdown_start,
+    input game_start,
+    input game_finish, 
+    output reg [1:0] game_select
+    );
+    
+    always @ (*) begin
+        if (countdown_start == 1) begin
+            game_select <= 2'b01;
+        end else if (game_start == 1) begin
+            game_select <= 2'b10;
+        end else if (game_finish == 1) begin
+            game_select <= 2'b11;
+        end //if
+    end //always
+endmodule
