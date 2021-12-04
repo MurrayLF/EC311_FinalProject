f6d8c34d8d1a2802ca89ff4fc2b8b0db02a32628
msisk23 <60163432+msisk23@users.noreply.github.com>
2021-12-04 17:13:48 -0500

    First implementation of top with all modules (not finished)

--- Top.v
+++ Top.v
@@ -36,6 +36,10 @@ module Top(
     wire        clock_14MHz_int;
     wire [1:0]  mode_int;
     wire        whacked_int;
+    wire        game_start;
+    wire [15:0] gametime_int;
+    wire [15:0] gamescore_int;
+    wire        game_finish;
     //wire [3:0]  thousands_int;
     //wire [3:0]  hundreds_int;
     //wire [3:0]  tens_int;
@@ -43,10 +47,16 @@ module Top(
     //wire [3:0]  display_int;
     //wire [3:0] display_out_int;
     
+    reg         countdown_start;
     reg         active_clock_int;
+    reg [1:0]   mode_selected;
+    reg         countdown_start;
+    reg [15:0]  display_int;
 
     ClockDivider clkdivide(clock_i, ~reset_i, clock_1Hz_int, clock_5Hz_int, clock_10Hz_int, clock_14MHz_int);
-    ModeSelection modeselect(buttons_i, mode_int);
+    ModeSelection modeselect(buttons_i, countdown_start, mode_int);
+    GameHandler gamehandle(countdown_start, game_start, game_finish, mode_selected);
+    CountDownHandler countdown(clock_1Hz_int, reset_i, gametime_int, game_start);
     
     always @ (*) begin
         case(mode_int)
@@ -57,26 +67,35 @@ module Top(
         endcase
     end //always
     
+    GameTimer gametime(clock_1Hz_int, reset_i, gametime_int, game_finish);
     MoleHandler molesetup(active_clock_int, clock_14MHz_int, ~reset_i, whacked_int, LEDs_o);
     WhackHandler whacking(~reset_i, LEDs_o, switches_i, whacked_int);
+    ScoreHandler scoring(whacked_int, game_start, gamescore_int);
     
-    //CountDownHandler countdown();
-    //ScoreHandler scoring();
-        
-    //if (~off)
-    //
-    //  if (in_game or post_game) display_int = score;
-    //  else if (in_count_down) display_int = count;
-    //
-    //  B2BCD b2bcd(display_int, thousands_int, hundreds_int, tens_int, ones_int);
-    //  SSDControl ssdctrl(clock_i, reset_i, thousands_int, hundreds_int, tens_int, ones_int, display_select_o, display_out_int);
-    //  BCD2SSD bcd2ssd(display_out_int, display_out_o);
-    //
-    // else
-    //
-    //  display_select = 4'b1111;
-    //  display_out = 7'b1111111;
-    //
-    // end
+    /*if (countdown_start == 1) begin
+        if (mode_int != 2'b00) begin
+            CountDownHandler countdown(clock_1Hz_int, reset_i, gametime_int, game_start);
+            if (game_start == 1) begin
+                GameTimer gametime(clock_1Hz_int, reset_i, gametime_int, game_finish);
+                MoleHandler molesetup(active_clock_int, clock_14MHz_int, ~reset_i, whacked_int, LEDs_o);
+                WhackHandler whacking(~reset_i, LEDs_o, switches_i, whacked_int);
+            end
+        end
+    end
+     //else
+    
+     //display_select = 4'b1111;
+     //display_out = 7'b1111111;
+    
+     //end*/
+
+    always @ (mode_selected) begin
+        if (mode_selected == 2'b01) display_int = gametime_int;
+        else if (mode_selected == 2'b10 || mode_selected == 2'b11) display_int = gamescore_int;
+    end 
+    
+    B2BCD b2bcd(display_int, thousands_int, hundreds_int, tens_int, ones_int);
+    SSDControl ssdctrl(clock_i, reset_i, thousands_int, hundreds_int, tens_int, ones_int, display_select_o, display_out_int);
+    BCD2SSD bcd2ssd(display_out_int, display_out_o);
     
 endmodule
