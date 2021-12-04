a6eb960a2610f324e3dbe60e6b0abf952a582303
Liam F Murray <55820110+MurrayLF@users.noreply.github.com>
2021-12-03 19:31:39 -0500

    Updated for decimal rather than hex encoding

--- SevenSegmentDisplay.v
+++ SevenSegmentDisplay.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 11/30/2021 12:27:27 AM
 // Design Name: 
-// Module Name: seven_segment_display
+// Module Name: SevenSegmentDisplay
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,30 +19,25 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// Verilog implementation of BCD to 7SD conversion
 module SevenSegmentDisplay(
-    input [3:0] display_out,
-    output reg [6:0] DigitN
+    input [3:0] display_in,
+    output reg [6:0]digit_o
     );
     
     always @(*) begin
-        case (display_out)
-            4'b0000: DigitN = 7'b1;
-            4'b0001: DigitN = 7'b1001111;
-            4'b0010: DigitN = 7'b0010010;
-            4'b0011: DigitN = 7'b0000110;
-            4'b0100: DigitN = 7'b1001100;
-            4'b0101: DigitN = 7'b0100100;
-            4'b0110: DigitN = 7'b0100000;
-            4'b0111: DigitN = 7'b0001111;
-            4'b1000: DigitN = 7'b0000000;
-            4'b1001: DigitN = 7'b0001100;
-            4'b1010: DigitN = 7'b0001000;
-            4'b1011: DigitN = 7'b0000000;
-            4'b1100: DigitN = 7'b0110001;
-            4'b1101: DigitN = 7'b0000001;
-            4'b1110: DigitN = 7'b0110000;
-            4'b1111: DigitN = 7'b0111000;
+        case (display_in)
+            4'b0000: digit_o = 7'b0000001;
+            4'b0001: digit_o = 7'b1001111;
+            4'b0010: digit_o = 7'b0010010;
+            4'b0011: digit_o = 7'b0000110;
+            4'b0100: digit_o = 7'b1001100;
+            4'b0101: digit_o = 7'b0100100;
+            4'b0110: digit_o = 7'b0100000;
+            4'b0111: digit_o = 7'b0001111;
+            4'b1000: digit_o = 7'b0000000;
+            4'b1001: digit_o = 7'b0001100;
+            default: digit_o = 7'b1111111;
         endcase
     end  
 endmodule
