cab85c3da73f1d801f2d6088a2d2ed0f7e62f5a9
msisk23 <60163432+msisk23@users.noreply.github.com>
2021-12-07 12:53:33 -0500

    Includes new 10 KHz clock

--- ClockDivider.v
+++ ClockDivider.v
@@ -27,25 +27,25 @@ module ClockDivider(
     output reg clock_1Hz_o,
     output reg clock_2Hz_o,
     output reg clock_5Hz_o,
-    output reg clock_14MHz_o
+    output reg clock_10KHz_o
     );
     
     reg [27:0]count_1Hz;
     reg [27:0]count_2Hz;
     reg [27:0]count_5Hz;
-    reg [2:0]count_14MHz;
+    reg [27:0]count_10KHz;
     
     parameter divisor_1Hz = 28'd100000000;
     parameter divisor_2Hz = 28'd50000000;
     parameter divisor_5Hz = 28'd20000000;
-    parameter divisor_14MHz = 3'd7;
+    parameter divisor_10KHz = 28'd10000;
    
     always @ (posedge clock_i) begin
         if (reset_i == 0) begin
             count_1Hz = 28'd0;
             count_2Hz = 28'd0;
             count_5Hz = 28'd0;
-            count_14MHz = 3'd0;
+            count_10KHz = 3'd0;
         end else begin
         
             if (count_1Hz >= divisor_1Hz/2) begin
@@ -63,16 +63,16 @@ module ClockDivider(
             end else begin
                 clock_5Hz_o = 1'b0;
             end //else
-            if (count_14MHz >= divisor_14MHz/2) begin
-                clock_14MHz_o = 1'b1;
+            if (count_10KHz >= divisor_10KHz/2) begin
+                clock_10KHz_o = 1'b1;
             end else begin
-                clock_14MHz_o = 1'b0;
+                clock_10KHz_o = 1'b0;
             end //else
             
             count_1Hz = count_1Hz + 1;
             count_2Hz = count_2Hz + 1;
             count_5Hz = count_5Hz + 1;
-            count_14MHz = count_14MHz + 1;
+            count_10KHz = count_10KHz + 1;
             
             if (count_1Hz == divisor_1Hz) begin
                 count_1Hz <= 28'd0;
@@ -83,11 +83,9 @@ module ClockDivider(
             if (count_5Hz == divisor_5Hz) begin
                 count_5Hz <= 28'd0;
             end //if
-            if (count_14MHz == divisor_14MHz) begin
-                count_14MHz <= 3'd0;
+            if (count_10KHz == divisor_10KHz) begin
+                count_10KHz <= 3'd0;
             end //if
         end //else
     end//always
 endmodule
-
-
