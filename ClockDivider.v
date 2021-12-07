40566756b133e323f7af63c1335feafd2a439f4e
Liam F Murray <55820110+MurrayLF@users.noreply.github.com>
2021-12-07 15:11:58 -0500

    Final commit

--- ClockDivider.v
+++ ClockDivider.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 11/29/2021 09:23:00 AM
+// Create Date: 11/29/2021 09:15:14 AM
 // Design Name: 
-// Module Name: ClockDivider
+// Module Name: TimeControl
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,86 +19,25 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-// Verilog implementation of 100MHz to 0.5Hz, 1Hz, 2Hz, 4Hz, 10Hz
-module ClockDivider(
-    input clock_i,
+//Verilog code for control of the time
+module TimeControl(
+    input clock_1Hz_i,
     input reset_i,
-    output reg clock_05Hz_o,
-    output reg clock_1Hz_o,
-    output reg clock_2Hz_o,
-    output reg clock_4Hz_o,
-    output reg clock_10KHz_o
+    output reg [15:0]count_o,
+    output reg [1:0]state_o
     );
     
-    reg [27:0]count_05Hz;
-    reg [27:0]count_1Hz;
-    reg [27:0]count_2Hz;
-    reg [27:0]count_4Hz;
-    reg [27:0]count_10KHz;
-    
-    parameter divisor_05Hz = 28'd200000000;
-    parameter divisor_1Hz = 28'd100000000;
-    parameter divisor_2Hz = 28'd50000000;
-    parameter divisor_4Hz = 28'd25000000;
-    parameter divisor_10KHz = 28'd10000;
-   
-    always @ (posedge clock_i) begin
-        if (reset_i == 0) begin
-            count_05Hz = 28'd0;
-            count_1Hz = 28'd0;
-            count_2Hz = 28'd0;
-            count_4Hz = 28'd0;
-            count_10KHz = 3'd0;
-        end else begin
-        
-            if (count_05Hz >= divisor_05Hz/2) begin
-                clock_05Hz_o = 1'b1;
-            end else begin
-                clock_05Hz_o = 1'b0;
-            end //else
-            if (count_1Hz >= divisor_1Hz/2) begin
-                clock_1Hz_o = 1'b1;
-            end else begin
-                clock_1Hz_o = 1'b0;
-            end //else
-            if (count_2Hz >= divisor_2Hz/2) begin
-                clock_2Hz_o = 1'b1;
-            end else begin
-                clock_2Hz_o = 1'b0;
-            end //else
-            if (count_4Hz >= divisor_4Hz/2) begin
-                clock_4Hz_o = 1'b1;
-            end else begin
-                clock_4Hz_o = 1'b0;
-            end //else
-            if (count_10KHz >= divisor_10KHz/2) begin
-                clock_10KHz_o = 1'b1;
-            end else begin
-                clock_10KHz_o = 1'b0;
-            end //else
-            
-            count_05Hz = count_05Hz + 1;
-            count_1Hz = count_1Hz + 1;
-            count_2Hz = count_2Hz + 1;
-            count_4Hz = count_4Hz + 1;
-            count_10KHz = count_10KHz + 1;
-            
-            if (count_05Hz == divisor_05Hz) begin
-                count_05Hz <= 28'd0;
-            end //if
-            if (count_1Hz == divisor_1Hz) begin
-                count_1Hz <= 28'd0;
-            end //if
-            if (count_2Hz == divisor_2Hz) begin
-                count_2Hz <= 28'd0;
-            end //if
-            if (count_4Hz == divisor_4Hz) begin
-                count_4Hz <= 28'd0;
-            end //if
-            if (count_10KHz == divisor_10KHz) begin
-                count_10KHz <= 3'd0;
-            end //if
-        end //else
-    end//always
+    always @ (posedge clock_1Hz_i or negedge reset_i) begin
+        if (reset_i == 1'b0) begin
+            count_o = 16'b0;
+        end else if(count_o < 16'd36) begin
+            count_o = count_o + 1'b1;
+        end // else
+    end // always
+
+    always @ (*) begin
+        if (count_o <= 16'd5) state_o = 2'b01;
+        else if (count_o <= 16'd35) state_o = 2'b10;
+        else state_o = 2'b11;
+    end //always
 endmodule
